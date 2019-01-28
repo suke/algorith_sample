c7d513509a642e6342a9d2bc9561a56deb9db7e8
suke <suke083.tech@gmail.com>
2019-01-28 21:56:08 +0900

    Add binary search

---
+++ binary_search.rb
@@ -0,0 +1,28 @@
+require 'benchmark'
+
+def binary_search(array, key)
+  low, high = 0, array.length - 1
+  count = 0
+  while low <= high
+    count += 1
+    mid = ((low + high) / 2).ceil
+    p "mid: #{mid}"
+    target = array[mid]
+    
+    if target == key
+      return mid
+    elsif target > key
+      high = mid - 1
+    else
+      low = mid + 1
+    end
+  end 
+  nil
+end
+
+puts Benchmark::CAPTION
+puts Benchmark.measure {
+  puts binary_search((1..10000000).to_a, 18465)  
+}
+
+
